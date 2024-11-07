18694f7bd4a63aed4dcc217b2b9868fb603687ae
raffaele <raffaele.fontana.02@gmail.com>
2024-11-07 23:44:39 +0100

    Implement main function with encoder and decoder integration

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+pub mod encoder;
+pub mod decoder;
+
+use encoder::Encoder;
+use decoder::Decoder;
+use std::io;
+
+
+fn main() -> io::Result<()> {
+
+    let img_path = "some_path.png";
+    let output_path = "modified_img.png";
+    let message = "Super secret message";
+
+    let encoder = Encoder::new(img_path, output_path, message);
+    encoder.encode()?;
+
+    let decoder = Decoder::new(output_path);
+    let decoded_message = decoder.decode();
+
+    println!("Decoded message: {}", decoded_message);
+
+    Ok(())
 }
